d81f4f46a97bd8aaef9f3c0e68f4a0a93fdced92
DivyeshPatel007 <dev.eth007@gmail.com>
2023-06-17 18:10:32 +0530

    Add Users Toogle

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../../assets/img/logo/dreamer.png"
 import userLogo from "../../assets/icons/user-line.svg"
 import "./Sidebar.scss"
@@ -6,16 +6,31 @@ import Timeline from '../Timeline/Timeline'
 import Navigation from '../Navigation/Navigation'
 
 function Sidebar() {
+  const [showUsers, setShowUsers] = useState(false)
+
+
+  function showUsersHandler() {
+
+    setShowUsers((prevState) => {
+      return !prevState
+    })
+  }
+
+  console.log(showUsers);
   return (
-    <div className='sidebar'>
+    <div className='sidebar' >
 
       <div className="sidebar-header">
         <div className="img-container">
           <img src={logo} alt="Company Logo" />
         </div>
-        <div className="sidebar-user__data">
+        <div className="sidebar-user__data" onClick={showUsersHandler}>
           <img src={userLogo} alt="" />
         </div>
+        {showUsers &&
+          <ul className='sidebar-users'>
+            <li onClick={showUsersHandler}>Patel Devanshu</li>  
+          </ul>}
       </div>
 
 
